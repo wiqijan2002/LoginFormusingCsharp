4e5b0bac97e5a345e6040e07b0ca1959da2303d3
waqas <wiqijan2002@yahoo.com>
2018-06-11 20:14:47 +0800

    different changes with redundancy removed

--- wiqilogin/createaccount.Designer.cs
+++ wiqilogin/createaccount.Designer.cs
@@ -78,7 +78,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(223, 22);
             this.textBox2.TabIndex = 4;
-            // 
+            
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(227, 170);
--- wiqilogin/createaccount.cs
+++ wiqilogin/createaccount.cs
@@ -26,25 +26,45 @@ namespace wiqilogin
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text != " " & textBox2.Text != " " & textBox3.Text != " ")
+            if (textBox1.Text == " " & textBox2.Text == " " & textBox3.Text == " ")
             {
-                string connetionString = null;
-                string sql = null;
-                connetionString = "Data Source=WAQASALI;Initial Catalog=login;Integrated Security=True";
-                using (SqlConnection cnn = new SqlConnection(connetionString))
+                MessageBox.Show("Please enter you details");
+            }
+            else
+            {
+                SqlConnection sc = new SqlConnection(@"Data Source = WAQASALI; Initial Catalog = login; Integrated Security = True");
+                string querry = "select * from Login Where Username ='"+textBox2.Text+ "'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, sc);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count > 0)
                 {
-                    sql = "insert into Login ( [Name], [Username], [Password]) values(@Name,@Username,@Password)";
-                    cnn.Open();
-                    using (SqlCommand cmd = new SqlCommand(sql, cnn))
-                    {
-                        cmd.Parameters.AddWithValue("Name",  textBox1.Text);
-                        cmd.Parameters.AddWithValue("Username", textBox2.Text);
-                        cmd.Parameters.AddWithValue("Password", textBox3.Text);
-                        cmd.ExecuteNonQuery();
-                        MessageBox.Show("Row inserted !! ");
+                    MessageBox.Show("Username is alreday taken");
+                }
 
-                       
+                try
+                {
+                    string connetionString = null;
+                    string sql = null;
+                    connetionString = "Data Source=WAQASALI;Initial Catalog=login;Integrated Security=True";
+                    using (SqlConnection cnn = new SqlConnection(connetionString))
+                    {
+                        sql = "insert into Login ( [Name], [Username], [Password]) values(@Name,@Username,@Password)";
+                        cnn.Open();
+                        using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                        {
+                            cmd.Parameters.AddWithValue("Name", textBox1.Text);
+                            cmd.Parameters.AddWithValue("Username", textBox2.Text);
+                            cmd.Parameters.AddWithValue("Password", textBox3.Text);
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Row inserted !! ");
+                        }
+
                     }
+
+                }
+                catch {
+                    MessageBox.Show("done");
                 }
             }
         }
